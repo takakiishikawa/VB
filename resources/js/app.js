7117c286e61b0abb33d49a5375239798e088ba5f
Takaki <rittukk1@gmail.com>
2024-03-18 22:04:23 +0700

    third

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
-import './bootstrap';
+import React, { Component } from 'react';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, React!</h1>
+        <p>Welcome to the React class component example.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
