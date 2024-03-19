584ea972e40f9f7d7234aa02ca5b0c19357d301b
Takaki <rittukk1@gmail.com>
2024-03-19 10:42:40 +0700

    first view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    @vite(['resources/js/Index.jsx'])
 </head>
 <body>
     <div id="app">
         <!-- Your application content here -->
     </div>
-
-    <!-- Include Vite client and app.js -->
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <script type="module" src="http://localhost:5173/app.js"></script>
 </body>
 </html>
