584caef7cefaef1b652d2d431c4c772d4fcaa48f
Takaki <rittukk1@gmail.com>
2024-03-20 18:10:37 +0700

    login/logout

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+  logout = () => {
+    axios.post('/logout',{},{
+      withCredentials: true
+    }).then(response => {
+      window.location.href = '/login';
+    }
+    ).catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello, React!</h1>
-        <p>Welcome to the React class component example.</p>
+        <h1>Page Title</h1>
+        <button onClick={this.logout}>Logout</button>
       </div>
     );
   }
