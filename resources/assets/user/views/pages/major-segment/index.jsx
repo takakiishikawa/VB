30c8ee339d4a2c9b90ff40a7edf9d377ade5b3c9
Takaki <rittukk1@gmail.com>
2024-04-05 16:43:09 +0700

    major_segment_ui

--- resources/assets/user/views/pages/major-segment/index.jsx
+++ resources/assets/user/views/pages/major-segment/index.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import HelpIcon from '@mui/icons-material/Help';
 import LockIcon from '@mui/icons-material/Lock';
-import LockOpenIcon from '@mui/icons-material/LockOpen';
+import LockOpenIcon from '@mui/icons-material/Lockopen';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import {Link} from 'react-router-dom';
@@ -24,46 +24,46 @@ class MajorSegment extends Component {
 
 
     iconCreate = (status) => {
-        return status == 1 ? <LockOpenIcon className={`major-segment__all__item-status-icon ${this.statusClass(status)}`} />
-            : status == 2 ? <CheckCircleOutlineIcon className={`major-segment__all__item-status-icon ${this.statusClass(status)}`} />
-                : <LockIcon className={`major-segment__all__item-status-icon ${this.statusClass(status)}`} />
+        return status == 1 ? <LockOpenIcon className={`major-segment__wrapper__item-status-icon ${this.statusClass(status)}`}  style={{fontSize: 25}} />
+            : status == 2 ? <CheckCircleOutlineIcon className={`major-segment__wrapper__item-status-icon ${this.statusClass(status)}`}  style={{fontSize: 25}} />
+                : <LockIcon className={`major-segment__wrapper__item-status-icon ${this.statusClass(status)}`}  style={{fontSize: 25}} />
     }
 
     render() {
         const {majorSegments, userMajorSegmentStatuses} = this.props;
-        console.log(majorSegments, "majorSegments");
-        console.log(userMajorSegmentStatuses, "userMajorSegmentStatuses");
-
         const activeMajorSegmentId = userMajorSegmentStatuses.find(item => item.status == 1)?.major_segment_id ?? null;
         return (
             <div>
                 <div className="major-segment">
-                    <div className="major-segment__open">
-                        <p className="major-segment__open__text">Open</p>
-                        <Link to={`/major-segment/${activeMajorSegmentId}`} className="major-segment__open__item">
-                            <LockOpenIcon className="major-segment__open__item-locked-icon" />
-                            <HelpIcon className="major-segment__open__item-help-icon" />
-                            <MenuBookIcon className="major-segment__open__item-book-icon" />
-                            <span className="major-segment__open__item-level">Level xx</span>
-                        </Link>
-                    </div>
-                    <div className="major-segment__all">
-                        <p className="major-segment__all__text">All</p>
-                            {majorSegments && majorSegments.map((majorSegment) => {
-                                //status1: unlocked, status2: completed, statusnull: locked
-                                const status = userMajorSegmentStatuses.find(item => item.major_segment_id === majorSegment.id)?.status ?? null;
-                                return (
-                                    <Link to={`/segment/${majorSegment.id}`} className="major-segment__all__item" key={majorSegment.id}>
-                                        {this.iconCreate(status)}
-                                        <HelpIcon className="major-segment__all__item-help-icon" />
-                                        <MenuBookIcon className={`major-segment__open__item-book-icon ${this.statusClass(status)}`} />
-                                         <span className="major-segment__all__item-level">Level {majorSegment.id}</span>
-                                    </Link>
-                                )
-                            })}
+                    <div className="major-segment__container">
+                        <div className="major-segment__wrapper">
+                            <div className="major-segment__wrapper__text">Active</div>
+                            <Link to={`/major-segment/${activeMajorSegmentId}`} className="major-segment__wrapper__item active">
+                                <LockOpenIcon className="major-segment__wrapper__item-status-icon unlocked" style={{fontSize: 35}} />
+                                <HelpIcon className="major-segment__wrapper__item-help-icon" style={{fontSize: 35}} />
+                                <MenuBookIcon className="major-segment__wrapper__item-book-icon unlocked" style={{fontSize: 115}} />
+                                <span className="major-segment__wrapper__item-level unlocked">Level {activeMajorSegmentId}</span>
+                            </Link>
+                        </div>
+                        <div className="major-segment__wrapper all">
+                            <div className="major-segment__wrapper__text">All</div>
+                            <div className="major-segment__wrapper__list">
+                                {majorSegments && majorSegments.map((majorSegment) => {
+                                    //status1: unlocked, status2: completed, statusnull: locked
+                                    const status = userMajorSegmentStatuses.find(item => item.major_segment_id === majorSegment.id)?.status ?? null;
+                                    return (
+                                        <Link to={`/segment/${majorSegment.id}`} className={`major-segment__wrapper__item ${this.statusClass(status)}`} key={majorSegment.id}>
+                                            {this.iconCreate(status)}
+                                            <HelpIcon className="major-segment__wrapper__item-help-icon" style={{fontSize: 25}} />
+                                            <MenuBookIcon className={`major-segment__wrapper__item-book-icon ${this.statusClass(status)}`}  style={{fontSize: 85}} />
+                                            <span className="major-segment__wrapper__item-level all">Level {majorSegment.id}</span>
+                                        </Link>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         );
     }
