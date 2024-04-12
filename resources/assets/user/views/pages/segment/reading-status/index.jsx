c89e499588bd45d374fbd002991749144fe560cd
Takaki <rittukk1@gmail.com>
2024-04-12 21:00:28 +0700

    segment_db_change

--- resources/assets/user/views/pages/segment/reading-status/index.jsx
+++ resources/assets/user/views/pages/segment/reading-status/index.jsx
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 
+
 class ReadingStatus extends Component {
     render() {
         return (
             <div>
                 <h1>Reading Status</h1>
+
+
+                
             </div>
         )
     }
 }
+
 
 export default ReadingStatus;
