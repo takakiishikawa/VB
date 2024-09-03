3318324db998c77d5389b74e4aca3b7199651b0d
Takaki <rittukk1@gmail.com>
2024-09-03 16:37:27 +0700

    test3

--- startup.sh
+++ startup.sh
@@ -3,7 +3,4 @@ cd /home/site/wwwroot
 php artisan config:cache
 php artisan route:cache
 # php-fpm is usually started by Azure, but we'll include it just in case
-if ! pgrep -x "php-fpm" > /dev/null
-then
-    php-fpm
-fi
+php-fpm
