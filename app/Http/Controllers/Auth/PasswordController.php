c86e87352befcfa0249ac092cb5ff92637ba80ed
Takaki <rittukk1@gmail.com>
2024-09-14 17:14:59 +0700

    test

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -19,7 +19,6 @@ class PasswordController extends Controller
             'current_password' => ['required', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
-
         $request->user()->update([
             'password' => Hash::make($validated['password']),
         ]);
